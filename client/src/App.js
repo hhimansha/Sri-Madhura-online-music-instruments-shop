9161c5693ec2057b6bc82ff32b520964b9a3bb56
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-04-17 02:24:49 +0530

    Add RentalOrdersManage component to AdminDash navigation menu

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,9 @@ import RentalItemDisplay from './components/Rental/RentalItemDisplay';
 import RentalManage from './components/Rental/RentalManage';
 import UpdateRental from './components/Rental/UpdateRental';
 import RentalItemPage from './components/Rental/RentalItemPage';
+import RentalOrdersManage from './components/Rental/RentalOrdersManage';
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -17,6 +19,7 @@ function App() {
           <Route path="/admindash/rentals/rentalcreate" element={<><AdminDash /><RentalItemCreate /></>} />
           <Route path="/admindash/rentals/update/:id" element={<><AdminDash /><UpdateRental /></>} />
           <Route path="/admindash/rentals" element={<><AdminDash /><RentalManage /></>} />
+          <Route path="/admindash/rental-orders" element={<><AdminDash /><RentalOrdersManage /></>} />
           <Route path="/rentals" element={<><TopNav/><RentHome /><RentalItemDisplay /><Footer/></>} />
           <Route path="/rentals/:id" element={<><TopNav/><RentalItemPage /><Footer/></>} />
           
