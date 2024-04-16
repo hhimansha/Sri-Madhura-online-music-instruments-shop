967e9178111810821d3b252ef9395a45cf159197
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-04-17 02:54:13 +0530

    Update RentalManage component to include FilterBar and adjust layout

--- client/src/components/Rental/RentalManage.jsx
+++ client/src/components/Rental/RentalManage.jsx
@@ -145,14 +145,15 @@ const RentalManage = () => {
 
     return (
         <div className="flex flex-col max-w-[1280px] mx-auto my-6 ml-[280px]">
-            <FilterBar
+            
+            <div className="flex mb-10 justify-between items-center">
+                <h1 className="text-2xl font-semibold leading-7 text-dark text-left">Manage Rentals</h1>
+                <FilterBar
                 categories={categories}
                 onFilterChange={setSelectedCategory}
                 onSortChange={setSelectedSort}
                 onSearch={setSearchQuery}
             />
-            <div className="flex mb-10 justify-between">
-                <h1 className="text-2xl font-semibold leading-7 text-dark text-left">Manage Rentals</h1>
                 <div className="flex">
                 <button onClick={downloadPDF} className="items-center text-sm font-medium rounded-lg text-dark hover:bg-gray-200 p-2 px-3 flex ">Download Report<span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
