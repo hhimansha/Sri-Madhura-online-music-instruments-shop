cdbac341fe1c006e6b88c58776adba90a4622396
Chanuri <164017419+chanuri2001@users.noreply.github.com>
2024-05-01 12:10:53 +0530

    repairs-adminHome

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TopNav from './components/topNav';
 import Footer from './components/Footer';
 import Home from './components/Repairs/Home'
+import HomeUser from './components/Repairs/HomeUser'
 import CreateRequest from './components/Repairs/CreateRequest'
 import EditRequest from './components/Repairs/EditRequest'
 import DeleteRequest from './components/Repairs/DeleteRequest'
@@ -20,7 +21,8 @@ function App() {
           <Route path = '/repair/create' element={<CreateRequest />} />
           <Route path = '/repair/edit/:id' element={<EditRequest />} />
           <Route path = '/repair/delete/:id' element={<DeleteRequest />} />
-          <Route path = '' element={<Home />} />
+          <Route path = '/repair/home' element={<Home />} />
+          <Route path = '/repair/homeUser' element={<HomeUser />} />
           <Route
            path="/admindash"
             element={<AdminDash />}
--- client/src/components/Repairs/Home.jsx
+++ client/src/components/Repairs/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Spinner from './Spinner';
+import Spinner from '../Repairs/Spinner';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
@@ -33,25 +33,12 @@ export const Home = () => {
     <TopNav/>
         <div className='p-4'>
             <div className='flex justify-center items-center gap-x-4'>
-  <button
-    className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-    onClick={() => setShowType('table')}
-    style={{ backgroundColor: '#de6418', color: '#fff' }}
-  >
-    Admin
-  </button>
-  <button
-    className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-    onClick={() => setShowType('card')}
-    style={{ backgroundColor: '#de6418', color: '#fff' }}
-  >
-    User
-  </button>
 
+  
 
             </div>
             <div className='flex justify-between items-center'>
-                <h1 className='text-3xl my-8'>Requests List</h1>
+                <h1 className='text-3xl my-8'>Customer Requests List</h1>
                 <Link to='/repair/create'>
                     <MdOutlineAddBox className='text-sky-800 text-4xl' />
                 </Link>
