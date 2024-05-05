2c2788816949485c61ce03689638e737b1af8588
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-05-05 15:11:21 +0530

    Merged with buddhi DONE

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user.js')
 const router = express.Router();
 const jwt = require('jsonwebtoken')
